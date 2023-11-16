70a87b8e7a56134562ece8a9334a572ce163de06
gabicilfone <cilfonegabriel@gmail.com>
2023-11-16 06:35:46 -0300

    Add get server side props

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+export default function Home({category}) {
+
+  console.log(category)
+  
   return (
     <h1>
       next js
     </h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const category = await prisma.category.findMany()
+
+  return {
+    props: {
+      category,
+    },
+  };
 }
