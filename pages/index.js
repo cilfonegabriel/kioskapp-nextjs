17b1a7c2e4964463fb5989862c77c594d2be09e7
gabicilfone <cilfonegabriel@gmail.com>
2023-11-20 10:07:08 -0300

    Add nem of category in the main page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../layout/Layout'
+import useKiosk from '../hooks/useKiosk'
 
 export default function Home() {
 
+  const { actualCategory } = useKiosk()
+
   return (
-    <Layout>
-      <h1>Inicio</h1>
+    <Layout page={`Menú ${actualCategory?.name}`}>
+      <h1 className='text-4xl font-black'>{actualCategory?.name}</h1>
     </Layout>
   )
 }
