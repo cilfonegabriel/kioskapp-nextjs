7bd7cf58ca2a0ba07293aef0b46651853a251ca1
gabicilfone <cilfonegabriel@gmail.com>
2023-11-21 16:14:30 -0300

    Add icon X and function for close the modal

--- components/ModalProduct.js
+++ components/ModalProduct.js
@@ -4,7 +4,7 @@ import {formatMoney} from "../helpers"
 
 const ModalProduct = () => {
 
-    const { product } = useKiosk()
+    const { product, handleChangeModal } = useKiosk()
 
   return (
     <div className="md:flex gap-10">
@@ -19,8 +19,24 @@ const ModalProduct = () => {
 
         <div className="md:w-2/3">
 
-            <div>
-                
+            <div className="flex justify-end">
+                <button
+                    onClick={handleChangeModal}
+                >
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        fill="none" viewBox="0 0 24 24" 
+                        strokeWidth={1.5} 
+                        stroke="currentColor" 
+                        className="w-6 h-6"
+                    >
+                        <path 
+                            strokeLinecap="round" 
+                            strokeLinejoin="round" 
+                            d="M6 18L18 6M6 6l12 12" 
+                        />
+                    </svg>
+                </button>
             </div>
             <h1 className="text-3xl font-bold mt-5">{product.name}</h1>
             <p className="mt-5 font-black text-5xl text-amber-500">
