76da2844ba0b947404a24b3186b2b73fa5175899
gabicilfone <cilfonegabriel@gmail.com>
2023-11-21 15:12:01 -0300

    Import helper and show the price

--- components/Product.js
+++ components/Product.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import {formatMoney} from "../helpers"
 
 const Product = ({product}) => {
 
@@ -9,6 +10,9 @@ const Product = ({product}) => {
       <Image src={`/assets/img/${image}.jpg`} alt={`Imagen Platillo ${name}`} width={400} height={500} />
       <div className="p-5">
         <h3 className="text-2xl font-bold">{name}</h3>
+        <p className="mt-5 font-black text-4xl text-amber-500">
+            {formatMoney(price)}
+        </p>
       </div>
     </div>
   )
