d427aac6173bab82c4d8b337141ad3e6fea992e2
gabicilfone <cilfonegabriel@gmail.com>
2023-11-21 14:49:35 -0300

    Add Eager Loading in Prisma

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -5,7 +5,11 @@ export default async function handler(req, res) {
   
   const prisma = new PrismaClient()
 
-  const category = await prisma.category.findMany();
+  const category = await prisma.category.findMany({
+    include: {
+      products: true
+    }
+  });
 
   res.status(200).json(category)
 }
--- pages/api/products.js
+++ pages/api/products.js
@@ -5,7 +5,11 @@ export default async function handler(req, res) {
   
   const prisma = new PrismaClient()
 
-  const products = await prisma.product.findMany();
+  const products = await prisma.product.findMany({
+    where: {
+        categoryId: 1,
+    }
+  });
 
   res.status(200).json(products)
 }
