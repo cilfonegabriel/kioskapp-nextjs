bdbd2cf24dd7f9d5b45fb0d08c8455d5a925acb8
gabicilfone <cilfonegabriel@gmail.com>
2023-11-21 15:00:19 -0300

    Add name to the image

--- components/Product.js
+++ components/Product.js
@@ -7,6 +7,9 @@ const Product = ({product}) => {
   return (
     <div className="border p-3">
       <Image src={`/assets/img/${image}.jpg`} alt={`Imagen Platillo ${name}`} width={400} height={500} />
+      <div className="p-5">
+        <h3 className="text-2xl font-bold">{name}</h3>
+      </div>
     </div>
   )
 }
