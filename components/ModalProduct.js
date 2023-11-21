80e382707cd437008babc2dc4637673ebdf1cf07
gabicilfone <cilfonegabriel@gmail.com>
2023-11-21 16:08:52 -0300

    Add name and price of product to the money

--- components/ModalProduct.js
+++ components/ModalProduct.js
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import useKiosk from "../hooks/useKiosk"
+import {formatMoney} from "../helpers" 
 
 const ModalProduct = () => {
 
@@ -17,7 +18,14 @@ const ModalProduct = () => {
         </div>
 
         <div className="md:w-2/3">
+
+            <div>
+                
+            </div>
             <h1 className="text-3xl font-bold mt-5">{product.name}</h1>
+            <p className="mt-5 font-black text-5xl text-amber-500">
+                {formatMoney(product.price)}
+            </p>
 
         </div>
     </div>
