048fd81bd91d2e6c2e7f59c3e0c5d70b6716f77c
gabicilfone <cilfonegabriel@gmail.com>
2023-11-21 14:58:34 -0300

    Import component of product

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../layout/Layout'
+import Product from '../components/Product'
 import useKiosk from '../hooks/useKiosk'
 
 export default function Home() {
@@ -13,6 +14,10 @@ export default function Home() {
       <p className='text-2xl my-10'>
         Choose and customize your order below
       </p>
+
+      {actualCategory?.products?.map(product =>(
+        <Product key={product.id} product={product} />
+      ))}
     </Layout>
   )
 }
