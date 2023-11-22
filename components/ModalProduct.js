b3cf0594caed617533f1cb0aabc3efc05146d513
gabicilfone <cilfonegabriel@gmail.com>
2023-11-22 10:01:01 -0300

    Add conditions of amounts

--- components/ModalProduct.js
+++ components/ModalProduct.js
@@ -45,7 +45,13 @@ const ModalProduct = () => {
                 {formatMoney(product.price)}
             </p>
             <div className="flex gap-4 mt-5">
-                <button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        if (amount <= 1) return
+                        setAmount(amount - 1 )
+                    }}
+                >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
@@ -53,7 +59,13 @@ const ModalProduct = () => {
 
                 <p className="text-3xl">{amount}</p>
                 
-                <button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        if(amount >= 5 ) return
+                        setAmount(amount + 1 )
+                    }}
+                >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
