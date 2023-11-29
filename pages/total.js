38fc16fc988e9657a0b13e144113e3d40d37ee6b
gabicilfone <cilfonegabriel@gmail.com>
2023-11-29 15:29:10 -0300

    Add Confirm order button field

--- pages/total.js
+++ pages/total.js
@@ -1,11 +1,27 @@
 import Layout from "../layout/Layout"
+import { useEffect } from "react";
+import useKiosk from "../hooks/useKiosk";
 
 export default function Total () {
+
+    const {order} = useKiosk()
+
+    const checkOrder = () => {
+        return order.length === 0
+    }
+
+    const addOrder = e => {
+        e.preventDefault();
+        console.log("Get order")
+    }
+
     return (
         <Layout page="Total and confirm your order">
             <h1 className="text-4xl font-black">Total and confirm your order</h1>
             <p className="text-2xl my-10">Confirm your order below</p>
-            <form>
+            <form
+                onSubmit={addOrder}
+            >
                 <div>
                     <label htmlFor="name" className="block uppercase text-slate-800 font-bold text-xl">Name</label>
 
@@ -21,6 +37,7 @@ export default function Total () {
                 </div>
                 <div className="mt-5">
                     <input
+                        type="submit"
                         className="bg-indigo-600 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center"
                         value="Confirm Order" 
                     />
