017809383ad4f0e25d94ef67a1f5a06d3604c3b3
gabicilfone <cilfonegabriel@gmail.com>
2023-12-01 06:29:43 -0300

    titles to the page admin

--- pages/admin.js
+++ pages/admin.js
@@ -4,7 +4,8 @@ export default function Admin() {
 
     return(
         <AdminLayout page={'Admin'}>
-            
+            <h1 className="text-4xl font-black">Administration panel</h1>
+            <p className="text-2xl my-10">Manage your Order</p>
         </AdminLayout>
     )
 }
