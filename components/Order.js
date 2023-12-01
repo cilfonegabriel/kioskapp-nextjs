b177f561b1e840e504759364c0da7954f9b43a78
gabicilfone <cilfonegabriel@gmail.com>
2023-12-01 07:43:54 -0300

    Show order and name in the admin panel

--- components/Order.js
+++ components/Order.js
@@ -1,7 +1,15 @@
-export default function Order() {
-  return (
-    <div>
-      Ordenes
-    </div>
-  )
+export default function Order({order}) {
+
+    const { id, name, total, pedido } = order
+
+    return (
+        <div className="border p-10 space-y-5">
+            <h3 className="text-2xl font-bold">Order: {id}</h3>
+            <p className="text-lg font-bold">Customer: {name}</p>
+
+            <div>
+                
+            </div>
+        </div>
+    )
 }
