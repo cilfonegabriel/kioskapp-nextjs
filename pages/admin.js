be28035f2f730a7c23508d1635bbabb12891c838
gabicilfone <cilfonegabriel@gmail.com>
2023-12-01 07:24:13 -0300

    Condition if data exists

--- pages/admin.js
+++ pages/admin.js
@@ -1,20 +1,23 @@
 import useSWR from "swr"
 import axios from "axios"
 import AdminLayout from "../layout/AdminLayout"
+import Order from "../components/Order"
 
 export default function Admin() {
 
     const fetcher = () => axios('/api/orders').then(datos => datos.data)
     const {data, error, isLoading} = useSWR('/api/orders', fetcher)
-    console.log(data)
-    console.log(error)
-    console.log(isLoading)
+
 
 
     return(
         <AdminLayout page={'Admin'}>
             <h1 className="text-4xl font-black">Administration panel</h1>
             <p className="text-2xl my-10">Manage your Order</p>
+
+            {data && data.length ? data.map(order => 
+            
+            ) :}
         </AdminLayout>
     )
 }
